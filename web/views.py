185854ced64e99d60a10a965f318336faa2a491d
Raymond Lee <10930328@dcsh.tp.edu.tw>
2020-12-24 11:18:42 +0800

    delete message

--- web/views.py
+++ web/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, DeleteView
 from django.urls import reverse_lazy
 from .models import Message
 
